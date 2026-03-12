e34549a31b6b7099e731bc7b8d846673de44104c
piruththiviraja <piruththiviraja@gmail.com>
2026-03-12 12:12:17 +0530

    mail config done using env

--- supabase/functions/send-contact-email/index.ts
+++ supabase/functions/send-contact-email/index.ts
@@ -28,11 +28,11 @@ Deno.serve(async (req) => {
       body: JSON.stringify({
         personalizations: [
           {
-            to: [{ email: "maruthesh12131@gmail.com" }]
+            to: [{ email: import.meta.env.VITE_TO_MAIL}]
           }
         ],
         from: {
-          email: "maruthesh12131@gmail.com"
+          email: import.meta.env.VITE_FROM_MAIL
         },
         subject: "New Contact Message",
         content: [
